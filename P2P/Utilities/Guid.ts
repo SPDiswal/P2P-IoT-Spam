c2d5793a545bf617403faa06f89f328ae616f25a
Steffen Diswal <steffen.diswal@gmail.com>
2015-04-23 12:43:31 +0200

    Updated Guid

--- P2P/Utilities/Guid.ts
+++ P2P/Utilities/Guid.ts
@@ -1,11 +1,29 @@
 ﻿class Guid
 {
-    public static Count = 41;
+    constructor(private guid: string)
+    {
+    }
 
+    public get id(): string
+    {
+        return this.guid;
+    }
+
+    // http://jsfiddle.net/briguy37/2MVFd/
     public static create(): Guid
     {
-        this.Count++;
-        return this.Count;
+        var guidTemplate = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
+        var time = new Date().getTime();
+
+        var guid = guidTemplate.replace(/[xy]/g, c =>
+        {
+            var r = (time + Math.random() * 16) % 16 | 0;
+            time = Math.floor(time / 16);
+            return (c === "x" ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+
+        return new Guid(guid);
     }
 }
+
 export = Guid;
