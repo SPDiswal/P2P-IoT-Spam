ecc17856fe439ec08f1252b0466b9ee3b280fefb
Steffen Diswal <steffen.diswal@gmail.com>
2015-05-13 13:15:35 +0200

    Moved Responsibility class.

---
+++ P2P/Common/Responsibility.ts
@@ -0,0 +1,16 @@
+﻿class Responsibility
+{
+    constructor(public identifier: string, public data: any)
+    {
+    }
+
+    public static deserialise(input: any): Responsibility
+    {
+        var responsibility = new Responsibility(null, null);
+        responsibility.identifier = input.identifier;
+        responsibility.data = input.data;
+        return responsibility;
+    }
+}
+
+export = Responsibility;
